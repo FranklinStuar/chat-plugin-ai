1b4afb6ab04f28f2d38ea079282b9d6f99ac2218
Franklin Peñafiel <franklinpenafiel1991@gmail.com>
2023-03-21 22:36:35 -0500

    init plugin form admin

---
+++ chat-ai.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Chat IA
+Plugin URI: https://github.com/FranklinStuar/chat-plugin-ai.git
+Description: Chat using IA for your company
+Version: 1.0.0
+Author: Franklin Peñafiel
+Author URI: https://franklindev.top/
+License: Attribution-NonCommercial-NoDerivatives (BY-NC-ND)
+License URI: https://creativecommons.org/licenses/by-nc-nd/4.0/
+*/
